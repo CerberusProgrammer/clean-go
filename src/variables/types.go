1524c0032f284ac2cbbc69cd374180759a7a66f3
Omar Flores <omar.desarrollo@gmtransporterp.com>
2024-12-17 14:42:22 -0800

    refactor VariableTypes function to utilize SimpleLayoutStructure for improved layout and content presentation

--- src/variables/types.go
+++ src/variables/types.go
@@ -1,18 +1,38 @@
 package variables
 
 import (
-	"fmt"
+	"clean-go/src/models"
+	"html/template"
 	"net/http"
 )
 
 func VariableTypes(w http.ResponseWriter) {
-	var str string = "Hello, Go!"
-	var num int = 42
-	var flag bool = true
-	var fnum float64 = 3.14
+	example := models.SimpleLayoutStructure{
+		Title: "Variable Types",
+		Body: `Go is a statically typed language, which means that the type of a variable is known at compile time.
 
-	fmt.Fprintf(w, "String: %s\n", str)
-	fmt.Fprintf(w, "Integer: %d\n", num)
-	fmt.Fprintf(w, "Boolean: %t\n", flag)
-	fmt.Fprintf(w, "Float: %f\n", fnum)
+The type of a variable determines the values that the variable can hold and the operations that can be performed on it.
+
+Go has the following basic types:
+
+1. bool
+2. string
+3. int, int8, int16, int32, int64
+4. uint, uint8, uint16, uint32, uint64, uintptr
+5. byte (alias for uint8)
+6. rune (alias for int32)
+7. float32, float64
+8. complex64, complex128
+`,
+		Examples: []string{
+			"var str string = \"Hello, Go!\"",
+			"var num int = 42",
+			"var flag bool = true",
+			"var fnum float64 = 3.14",
+		},
+	}
+
+	tmpl := template.Must(template.ParseFiles("templates/simple_layout.html"))
+	w.Header().Set("Content-Type", "text/html")
+	tmpl.Execute(w, example)
 }
